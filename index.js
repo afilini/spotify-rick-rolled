3c3b47b2c8afff0784d36a2ac2dec97918fe4aff
junderw <junderwood@bitcoinbank.co.jp>
2019-03-08 13:53:28 +0900

    Better loop

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const SpotifyWebApi = require('spotify-web-api-node');
 
 const album = 'spotify:album:6N9PS4QXF1D0OWPk0Sxtb4';
 
+// This time interval is the time in ms between
+// the end of one loop and the beginning of the next loop
+const TIME_INTERVAL_MS = 100;
+
+function sleep(ms) { return new Promise(r => setTimeout(r, ms)) }
+
 const code = JSON.parse(fs.readFileSync(path.join(__dirname, 'code')).toString());
 const credentials = require('./credentials');
 
@@ -15,42 +21,37 @@ spotifyApi.setRefreshToken(code.refresh);
 
 let last_progress = 0;
 
-let done = true;
-
 async function loop() {
-    if (!done) {
-        return;
-    }
+    try {
+        let currentStatus = (await spotifyApi.getMyCurrentPlaybackState({})).body;
 
-    done = false;
-    let currentStatus = (await spotifyApi.getMyCurrentPlaybackState({})).body;
+        if (currentStatus && currentStatus.device) {
+            if (currentStatus.item.id !== '4uLU6hMCjMI75M1A2tKUQC') {
+                await spotifyApi.play({
+                    context_uri: album
+                });
 
-    if (currentStatus && currentStatus.device) {
-        if (currentStatus.item.id !== '4uLU6hMCjMI75M1A2tKUQC') {
-            await spotifyApi.play({
-                context_uri: album
+                await spotifyApi.seek(last_progress);
+
+                console.log(new Date(), "You got RickRoll'd!");
+            } else if (!currentStatus.is_playing) {
+                await spotifyApi.play();
+
+                console.log(new Date(), "Nice try :)");
+            } else {
+                last_progress = currentStatus.progress_ms;
+            }
+
+            await spotifyApi.setRepeat({
+                state: 'track'
             });
-
-            await spotifyApi.seek(last_progress);
-
-            console.log(new Date(), "You got RickRoll'd!");
-        } else if (!currentStatus.is_playing) {
-            await spotifyApi.play();
-
-            console.log(new Date(), "Nice try :)");
-        } else {
-            last_progress = currentStatus.progress_ms;
         }
-
-        await spotifyApi.setRepeat({
-            state: 'track'
-        });
+    } catch (error) {
+        console.error(error)
+        await sleep(5000) // wait 5 seconds just in case
+    } finally {
+        setTimeout(() => { loop().catch(console.error); }, TIME_INTERVAL_MS);
     }
-
-    done = true;
 }
 
-setInterval(function () {
-    loop()
-        .catch(e => console.log(e));
-}, 600);
+loop().catch(console.error);
